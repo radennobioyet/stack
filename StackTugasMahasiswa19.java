0bd6bcd08a3d33f86ca9866a75b3532fd9e661b2
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2026-04-22 10:27:49 +0700

    5

--- StackTugasMahasiswa19.java
+++ StackTugasMahasiswa19.java
@@ -63,6 +63,10 @@ public class StackTugasMahasiswa19 {
         }
     }
 
+    public int getCount() {
+        return top + 1;
+    }
+
     public void print() {
         for (int i = top; i >= 0; i--) {
             System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
